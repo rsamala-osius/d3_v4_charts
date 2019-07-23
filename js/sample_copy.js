cedb1e29bb7da0906ff9b9f83ff853b9b8dd28f7
Raju Samala <rsamala.osicus@gmail.com>
2019-07-23 12:57:19 +0530

    22-July-2019

--- js/sample_copy.js
+++ js/sample_copy.js
@@ -6,9 +6,7 @@
 //	var chartSize = 600;
 //	var width = chartSize , height = chartSize;
 
-
-
-var width = 600, height = 400;
+var width = 600, height = 600;
 
 var centre = { x: width / 2, y: height / 2 };
 var forceStrength = 0.03;
@@ -26,23 +24,22 @@ var radiusScale = d3.scaleSqrt().domain([1, 600]).range([10, 80]);
 
 // Simulation is collection of forces about where we want to our circles to go
 var simulation = d3.forceSimulation()
-.force('x', d3.forceX(width/2).strength(0.05))
-.force('y', d3.forceY(height/2).strength(0.05))	.force('collision', d3.forceCollide(function (d) {
-  return radiusScale(d.radius) + 5;
-}));
+  .force('x', d3.forceX().strength(forceStrength).x(centre.x))
+  .force('y', d3.forceY().strength(forceStrength).y(centre.y))
+  .force('collision', d3.forceCollide().radius( function(d) { return d.radius + 2 }));
 
 simulation.stop();
 
 
 function createNodes(rawData, noOfValues) {
-  rawData = rawData.sort( (y, x) => x.count.total_effort - y.count.total_effort ).slice(0,noOfValues);
+  rawData = rawData.sort(function (y, x) { return x.count.total_effort - y.count.total_effort } ).slice(0,noOfValues);
   // use max size in the data as the max in the scale's domain
   // note we have to ensure that size is a number
   var maxSize = d3.max(rawData, function(d) { return  +d.count.total_effort });
   // size bubbles based on area
   var radiusScale = d3.scaleSqrt()
-  .domain([0, maxSize])
-  .range([0, 150])
+    .domain([0, maxSize])
+    .range([0, 150])
 
   // use map() to convert raw data into node data
   var myNodes = rawData.map(function(d) {
@@ -59,29 +56,30 @@ function createNodes(rawData, noOfValues) {
 }
 
 function ready(err, dataPoints) {
-  dataPoints = createNodes(dataPoints, 5);
+  // dataPoints = createNodes(productData, 5);
+  dataPoints = createNodes(dataPoints, 10);
   var averageValue = d3.mean(dataPoints, function(d) { return  +d.size });
 
   svg = d3.select('#chart')
-  .append('svg')
-  .attr('height', height)
-  .attr('width', width)
-  .append('g')
-  .attr('transform', 'translate(0,0)');
+    .append('svg')
+    .attr('height', height)
+    .attr('width', width)
+    .append('g')
+    .attr('transform', 'translate(0,0)');
 
   var circles = svg.selectAll('.artist')
-  .data(dataPoints, function(d) { return d.name })
-  .enter()
-  .append('g')
+    .data(dataPoints, function(d) { return d.name })
+    .enter()
+    .append('g')
 
   var tip = d3.tip()
-  .attr('class', 'd3-tip')
-  .offset([-10, 0])
-  .html(function(d) {
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(d) {
 
-    return "<strong> Product:</strong> <span style='color:#81B5A1'>" + d.name + "</span>" +
-      " <br /> <sgrong>Effort :</sgrong> <span style='color:#81B5A1'> " + d.size + "</span>";
-  });
+      return "<strong> Product:</strong> <span style='color:#81B5A1'>" + d.name + "</span>" +
+        " <br /> <sgrong>Effort :</sgrong> <span style='color:#81B5A1'> " + d.size + "</span>";
+    });
 
   svg.call(tip);
 
@@ -89,24 +87,24 @@ function ready(err, dataPoints) {
     .append('circle')
     .classed('bubble', true)
     .attr('r', function (d) {
-    return d.radius;
-  })
+      return d.radius;
+    })
     .attr('fill', function (d) {
-    if( d.size <= averageValue && d.size > 10) {
-      return '#65adb2';
-    } else if ( d.size > averageValue && d.size > (2 *averageValue) ) {
-      return '#81B5A1';
-    }
-    else if(d.size < 5){
-      return '#8686BC';
-    }
-    else if(d.size <= 10 && d.size >=5){
-      return '#72D0E2 ';
-    }
-    return '#293E40';
+      if( d.size <= averageValue && d.size > 10) {
+        return '#65adb2';
+      } else if ( d.size > averageValue && d.size > (2 *averageValue) ) {
+        return '#81B5A1';
+      }
+      else if(d.size < 5){
+        return '#8686BC';
+      }
+      else if(d.size <= 10 && d.size >=5){
+        return '#72D0E2 ';
+      }
+      return '#293E40';
 
-    // return myColor(d.size);
-  })
+      // return myColor(d.size);
+    })
     .on('mouseover', tip.show)
     .on('mouseout', tip.hide);
 
@@ -133,5 +131,5 @@ function ready(err, dataPoints) {
   }
 }
 
-d3.json('http://localhost:8081/get_davies', ready);
+d3.json('http://localhost:8081/get_davies', ready)
 // d3.queue().await(ready);
